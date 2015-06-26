606c4423290f13528ac7fafdad742a17d166ae55
Danielle <danielleheckman5@gmail.com>
2015-06-26 15:53:08 -0400

    updated readme, alert people when meetings are coming up

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ###Objective
 My objective was to take a wireframe of FakeCo's meeting schedule and turn it into a working prototype. The main goal was to make sure all information regarding meetings was viewable at a glance, as they were no peripherals attached.
 
-I stuck to basic HTML/CSS/JavaScript as well as JQuery.
+I stuck to basic HTML/CSS/JavaScript as well as JQuery and Moment.js for time.
+
--- js/script.js
+++ js/script.js
@@ -1,54 +1,54 @@
 // current time
-    var interval = setInterval(function() {
-        var momentNow = moment();
-        $('#time').html(momentNow.format(' hh:mm A'));
-    }, 100);
+var interval = setInterval(function() {
+	var momentNow = moment();
+	$('#time').html(momentNow.format(' hh:mm A'));
+}, 100);
 
 
 //ajax call to JSON api
 var getMeetings = function() {
 	$.ajax({
-		dataType: "jsonp",
 		url: "http://fake-co-calendar.herokuapp.com/api/v1/events",
+		dataType: "jsonp",
 		success: function(data) {
 			var meetings = data.events.list;
-			var allMeetings = $.map(meetings, function (meeting) {
+			var allMeetings = $.map(meetings, function(meeting) {
       	var attendees = " ";
-        $.each(meeting.attendees, function (index) {
+        $.each(meeting.attendees, function(index) {
         	var person = meeting.attendees[index]
           attendees += "<li><img src='" + person.photo +"'></li>";
         });
 
         // cueing audience when meetings are about to start
-        var meetingTime = moment(meeting.start_time).subtract(10,'minutes').format('hh:mm');
+        var meetingCue = "Beginning " + moment(meeting.start_time).fromNow();
         var currentTime = moment().format('hh:mm');
-        var meetingAlert = function() {
-        	if (meetingTime < currentTime){
-        		return "you gotta go";
-        	};
-        }
+        var meetingAlert = function(s) {
+        	var meetingSoon = moment(s).subtract(15, 'minutes');
+         	if (meetingSoon > moment()) {
+            return meetingCue;
+					}
+        	else {
+        		return '';
+        	}
+        };
        
+			 	// adding to HTML
+			 	var meetingTime = "<div class='meeting-alert'>" + meetingAlert(meeting.start_time, meeting.end_time) + "</div>" 
+			 	+ "<div class='meeting-time'>"  + moment(meeting.start_time).format('h:mm') 
+			 	+ "-" + moment(meeting.end_time).format('h:mmA') + "</div>";	
+			 	var meetingName = "<div class='meeting-name'>" + meeting.name + "</div>";
+			 	var meetingLocation = "<div class='meeting-location'>" + meeting.location + "</div>";
+			 	var meetingAttendees = "<ul class='people'><div class='photos'>" + attendees + "</div></ul>";	 
 
-
-			 	// adding to HTML			 
-			 	return "<div class='meeting'><ul class='info'><li>" + 
-			 	"<div class='meeting-time'>" + 
-			 	moment(meeting.start_time).format('h:mm') + "-"
-			 	+ moment(meeting.end_time).format('h:mmA') + "</div>" +
-			 	"<div class='meeting-name'>" + meeting.name + "</div>" + 
-			 	"<div class='meeting-location'>" + meeting.location + 
-			 	"</div></ul>" + "<ul class='people'><div class='photos'>" 
-			 	+ attendees + "</div></ul></li></div>" +	"<div class='soon'>" 
-			 	+  "</div>"
+			 	return "<div class='meeting'><ul class='info'><li>" + meetingTime + meetingName 
+			 	+ meetingLocation + "</li></ul>" +meetingAttendees + "</div>"
+			 	
  		 	
 			});
 
-			$('.meetings').append(allMeetings.join('')) 
+			$('.meetings').append(allMeetings) 
 	  }
  	});
-
- 
-
 };
 
 getMeetings();
